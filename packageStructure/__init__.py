fd16182779446236c9b17fd17e25bc1e5c71f33b
agosling <andrew.gosling@nhs.net>
2021-02-04 15:29:16 +0000

    __init__.py template

---
+++ packageStructure/__init__.py
@@ -0,0 +1,7 @@
+'''
+All packages within this folder structure can be called
+
+In this file, list the functions in each file in the following structure:
+
+from [folder]...[folder].[filename] import [function]
+'''
